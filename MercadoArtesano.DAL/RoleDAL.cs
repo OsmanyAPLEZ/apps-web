847683a1ce431a6f35ef77ca4fbfe544f650aa18
Osmany Lopez <osmany203lopez@gmail.com>
2024-04-29 22:59:53 -0600

    Programacion de Role DAL

--- MercadoArtesano.DAL/RoleDAL.cs
+++ MercadoArtesano.DAL/RoleDAL.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MercadoArtesano.EN;
+using Microsoft.EntityFrameworkCore;
 
 namespace MercadoArtesano.DAL
 {
