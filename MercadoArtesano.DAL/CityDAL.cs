a767e7a6458ac697473aa696f0d40106a2d0b9e3
Osmany Lopez <osmany203lopez@gmail.com>
2024-05-01 18:29:15 -0600

    Programacion CityDAL

--- MercadoArtesano.DAL/CityDAL.cs
+++ MercadoArtesano.DAL/CityDAL.cs
@@ -27,5 +27,99 @@ namespace MercadoArtesano.DAL
         }
         #endregion
 
+        #region Metodo de Buscar Registro
+        // IQueryable es una interfaz que toma un coleccion a la cual se le pueden implementar multiples consultas (Filtros) 
+        internal static IQueryable<City> QuerySelect(IQueryable<City> query, City pCity)
+        {
+
+            // Por ID
+            if (pCity.Id > 0)
+                query = query.Where(r => r.Id == pCity.Id);
+
+
+            // Por Nomnbre, Si es verdadero lo vuelve falso y viceversa 
+            if (!string.IsNullOrWhiteSpace(pCity.Name))
+                query = query.Where(r => r.Name.Contains(pCity.Name));
+
+            query = query.OrderByDescending(r => r.Id).AsQueryable();
+
+            // Para la cantidad de registros a mostrar
+            if (pCity.Top_Aux > 0)
+                query = query.Take(pCity.Top_Aux).AsQueryable();
+
+            return query;
+        }
+        public static async Task<List<City>> SearchAsync(City pCity)
+        {
+            var cities = new List<City>();
+            using (var dbContext = new ContextDB())
+            {
+                var select = dbContext.Cities.AsQueryable();
+                select = QuerySelect(select, pCity);
+                cities = await select.ToListAsync();
+            }
+            return cities;
+        }
+        #endregion
+
+        #region Metodo para Modificar 
+        public static async Task<int> UpdateAsync(City city)
+        {
+            int result = 0;
+            using (var dbContext = new ContextDB())
+            {
+                var cityDb = await dbContext.Cities.FirstOrDefaultAsync(c => c.Id == city.Id);
+                if (cityDb != null)
+                {
+                    cityDb.Name = city.Name;
+                    dbContext.Cities.Update(cityDb);
+                    result = await dbContext.SaveChangesAsync();
+                }
+            }
+            return result;
+        }
+        #endregion
+
+        #region Metodo para Eliminar 
+        public static async Task<int> DeleteAsync(City city)
+        {
+            int result = 0;
+            using (var dbContext = new ContextDB())
+            {
+                var cityDb = await dbContext.Cities.FirstOrDefaultAsync(c => c.Id == city.Id);
+                if (cityDb != null)
+                {
+                    dbContext.Cities.Remove(cityDb);
+                    result = await dbContext.SaveChangesAsync();
+                }
+            }
+            return result;
+        }
+        #endregion
+
+        #region Metodo para Obtener Id 
+        public static async Task<City> GetByIdAsync(City city)
+        {
+            var cityDb = new City();
+            using (var dbContext = new ContextDB())
+            {
+                cityDb = await dbContext.Cities.FirstOrDefaultAsync(c => c.Id == city.Id);
+            }
+            return cityDb!;
+        }
+        #endregion
+
+        #region Mostrar todos
+        public static async Task<List<City>> GetAllAsync()
+        {
+            var cities = new List<City>();
+            using (var dbContext = new ContextDB())
+            {
+                cities = await dbContext.Cities.ToListAsync();
+            }
+            return cities;
+        }
+        #endregion
+
     }
 }
