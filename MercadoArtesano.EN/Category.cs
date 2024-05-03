83dd061985b2556b7710f01e910b560cecd8777a
Osmany Lopez <osmany203lopez@gmail.com>
2024-05-02 20:20:13 -0600

    Programacion categoria EN

---
+++ MercadoArtesano.EN/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MercadoArtesano.EN
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(50, ErrorMessage = "El largo máximo es de 50 caracteres")]
+        [Display(Name = "Nombre")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", ErrorMessage = "El Nombre debe contener solo Letras")] // Validamos el tipo de dato
+        public string Name { get; set; } = string.Empty;
+
+        [NotMapped]
+        public int Top_Aux { get; set; } // propiedad auxiliar
+
+        // public List<Product>? Products { get; set; } // propiedad de navegación  Esta propiedad representa la relación uno a muchos y permite el acceso
+
+    }
+}
