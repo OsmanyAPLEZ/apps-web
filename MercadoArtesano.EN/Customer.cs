439bbfbf3fb32ba0b4d82517fc1ce057afae1638
Osmany Lopez <osmany203lopez@gmail.com>
2024-05-02 20:19:58 -0600

    Programacion cliente EN

--- MercadoArtesano.EN/Customer.cs
+++ MercadoArtesano.EN/Customer.cs
@@ -11,9 +11,14 @@ namespace MercadoArtesano.EN
 {
     public class Customer
     {
-        #region Entity's Attribute
+        #region Personal Data
         [Key]
         public int Id { get; set; }
+
+        [ForeignKey("Role")] //Indica que es una llave Foranea
+        [Required(ErrorMessage = "El rol es requerido")] //Indica que es un campo requerido
+        [Display(Name = "Rol")] // Una tipo traduccion (esto lo vera el cliente) 
+        public int IdRole { get; set; }
 
         [Required(ErrorMessage = "El nombre es requerido")] //Indica que es un campo requerido
         [StringLength(50, ErrorMessage = "Maximo 50 caracteres")] // Indica la longitud maxima para dicho campo
@@ -27,10 +32,10 @@ namespace MercadoArtesano.EN
         [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", ErrorMessage = "Debe contener solo Letras")] // Validamos el tipo de dato
         public string LastName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "El correo es requerido")] //Indica que es un campo requerido
-        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")] // Indica la longitud maxima para dicho campo
-        [Display(Name = "Correo")] // Una tipo traduccion (esto lo vera el cliente) 
-        public string EMail { get; set; } = string.Empty;
+        [ForeignKey("City")] //Indica que es una llave Foranea
+        [Required(ErrorMessage = "El Departamento es requerido")] //Indica que es un campo requerido
+        [Display(Name = "Departamento")] // Una tipo traduccion (esto lo vera el cliente) 
+        public string IdCity { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "El Telefono es requerido")] //Indica que es un campo requerido
         [StringLength(9, ErrorMessage = "Maximo 9 caracteres")] // Indica la longitud maxima para dicho campo
@@ -38,17 +43,30 @@ namespace MercadoArtesano.EN
         [RegularExpression("^[0-9-]+$", ErrorMessage = "El Telefono debe contener solo números")] // Validamos el tipo de dato
         public string Phone { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "La Direccion es requerido")] //Indica que es un campo requerido
-        [StringLength(100, ErrorMessage = "Maximo 100 caracteres")] // Indica la longitud maxima para dicho campo
-        [Display(Name = "Direccion")] // Una tipo traduccion (esto lo vera el cliente)
-        public string Adress { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "El nombre es requerido")] //Indica que es un campo requerido
+        [Required(ErrorMessage = "El correo es requerido")] //Indica que es un campo requerido
         [StringLength(50, ErrorMessage = "Maximo 50 caracteres")] // Indica la longitud maxima para dicho campo
-        [Display(Name = "Ciudad")] //Una tipo traduccion (esto lo vera el cliente) 
-        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", ErrorMessage = "Debe contener solo Letras")] // Validamos el tipo de dato
-        public string IdCity { get; set; } = string.Empty;
+        [Display(Name = "Correo")] // Una tipo traduccion (esto lo vera el cliente) 
+        public string EMail { get; set; } = string.Empty;
+        #endregion
 
+        #region User Data 
+        [Required(ErrorMessage = "El nombre de Usuario es requerido")] //Indica que es un campo requerido
+        [StringLength(20, ErrorMessage = "Maximo 20 caracteres")] // Indica la longitud maxima para dicho campo
+        [Display(Name = "Nombre de Usuario")] // Una tipo traduccion (esto lo vera el cliente) 
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ. ]+$", ErrorMessage = "El Nombre debe contener solo Letras")] // Validamos el tipo de dato
+        public string Login { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contrasena de Usuario es requerido")] //Indica que es un campo requerido
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")] // Indica la longitud maxima para dicho campo
+        [Display(Name = "Nombre de Usuario")] // Una tipo traduccion (esto lo vera el cliente) 
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        [Display(Name = "Estado")]
+        public byte Status { get; set; }
+        #endregion
+
+        #region Dates
         [Required(ErrorMessage = "La Fecha de Creacion es requerida")] //Indica que es un campo requerido
         [Display(Name = "Fecha de Creacion")] // Una tipo traduccion (esto lo vera el cliente) 
         [DataType(DataType.Date, ErrorMessage = "Por favor, introduce una fecha válida")]
@@ -60,32 +78,7 @@ namespace MercadoArtesano.EN
         public DateTime ModificationDate { get; set; } = DateTime.MinValue;
         #endregion
 
-        #region User Data 
-        [Required(ErrorMessage = "El nombre de Usuario es requerido")] //Indica que es un campo requerido
-        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")] // Indica la longitud maxima para dicho campo
-        [Display(Name = "Nombre de Usuario")] // Una tipo traduccion (esto lo vera el cliente) 
-        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ. ]+$", ErrorMessage = "El Nombre debe contener solo Letras")] // Validamos el tipo de dato
-        public string Login { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "La contrasena de Usuario es requerido")] //Indica que es un campo requerido
-        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")] // Indica la longitud maxima para dicho campo
-        [Display(Name = "Nombre de Usuario")] // Una tipo traduccion (esto lo vera el cliente) 
-        public string Password { get; set; } = string.Empty;
-
-        [ForeignKey("Rol")] //Indica que es una llave Foranea
-        [Required(ErrorMessage = "El rol es requerido")] //Indica que es un campo requerido
-        [Display(Name = "Rol")] // Una tipo traduccion (esto lo vera el cliente) 
-        public int IdRole { get; set; }
-
-        [Required(ErrorMessage = "El estado es requerido")]
-        [Display(Name = "Estado")]
-        public byte Status { get; set; }
-        #endregion
-
-        #region Not Mapped Attribute
-        [NotMapped]
-        public int Top_Aux { get; set; } // propiedad auxiliar
-
+        #region Not Mapped Data
         [NotMapped]
         [Required(ErrorMessage = "La confirmación de la contraseña es requerida")]
         [DataType(DataType.Password)]
@@ -100,10 +93,13 @@ namespace MercadoArtesano.EN
 
         [NotMapped]
         public City? City { get; set; } // Propiedad de Navegacion
+
+        [NotMapped]
+        public int Top_Aux { get; set; } // propiedad auxiliar
         #endregion
     }
 
-    public enum User_Status
+    public enum Customers_Status
     {
         ACTIVO = 1, INACTIVO = 2
     }
