2a167102f7365c358f1fb474c723c257422c1ccc
Osmany Lopez <osmany203lopez@gmail.com>
2024-05-02 20:20:53 -0600

    Programacion categoria BL

---
+++ MercadoArtesano.BL/CategoryBL.cs
@@ -0,0 +1,43 @@
+﻿using MercadoArtesano.DAL;
+using MercadoArtesano.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MercadoArtesano.BL
+{
+    public class CategoryBL
+    {
+        public async Task<int> CreateAsync(Category category)
+        {
+            return await CategoryDAL.CreateAsync(category);
+        }
+
+        public async Task<int> UpdateAsync(Category category)
+        {
+            return await CategoryDAL.UpdateAsync(category);
+        }
+
+        public async Task<int> DeleteAsync(Category category)
+        {
+            return await CategoryDAL.DeleteAsync(category);
+        }
+
+        public async Task<Category> GetByIdAsync(Category category)
+        {
+            return await CategoryDAL.GetByIdAsync(category);
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await CategoryDAL.GetAllAsync();
+        }
+
+        public async Task<List<Category>> SearchAsync(Category category)
+        {
+            return await CategoryDAL.SearchAsync(category);
+        }
+    }
+}
